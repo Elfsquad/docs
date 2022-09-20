82121671226eda4b9d71b7e6a2a7308397aca5c4
johannesheesterman <jheesterman@outlook.com>
2022-09-20 14:32:13 +0200

    clarity

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,12 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "dqkca1do5w");
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
