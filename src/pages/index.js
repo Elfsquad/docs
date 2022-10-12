489769e4e58a8d7cd39a9ce0e87f783113d6cc49
Johannes Heesterman <johannes@elfsquad.io>
2022-10-12 21:22:07 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,14 +28,6 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
 
-  useEffect(() => {
-    (function (c, l, a, r, i, t, y) {
-      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "dqkca1do5w");
-  }, []);
-
   return (
     <Layout
       title={`${siteConfig.title}`}
