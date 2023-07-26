8bf71666e2552eeb5a0cdc158ffa8d987f87d4a0
Stan van Rooy <me@stanvanrooy.com>
2023-07-26 14:08:40 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,13 +93,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/elfsquad/docs/edit/master/',
+            'https://github.com/elfsquad/docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/elfsquad/docs/edit/master/',
+            'https://github.com/elfsquad/docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
