a9e6ee6b9e54f9c6fb06272b74c0db45fad0fafc
Stan van Rooy <me@stanvanrooy.com>
2024-03-19 19:26:16 +0100

    fix: broken links

--- docs/customization/sidebar.ts
+++ docs/customization/sidebar.ts
@@ -1,10 +1,24 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+import methods from "./methods";
+
+const methodEntries = methods.map(({methodName}) => ({
+  type: 'link',
+  href: `/docs/customization/custom-scripting-api#${methodName}`,
+  label: methodName,
+}));
 
 const sidebar: SidebarsConfig = {
   apisidebar: [
     { type: 'doc', id: 'customization/index' },
     { type: 'doc', id: 'customization/getting-started' },
-    { type: 'doc', id: 'customization/custom-scripting-api' },
+    {
+      type: "category",
+      label: "Custom Scripting API",
+      items: [
+        { type: 'doc', id: 'customization/custom-scripting-api' },
+        ...methodEntries,
+      ],
+    },
     { type: 'doc', id: 'customization/examples' },
   ],
 };
--- docs/guides/Introduction.md
+++ docs/guides/Introduction.md
@@ -5,6 +5,4 @@ sidebar_label: Introduction
 slug: ./
 ---
 
-* [Embedding the showroom](./embed-showroom)
-* [Generating 2d step images](./generating-2d-step-images)
 * [Retrieve all quotation lines with an article code](./retrieve-quotation-lines-with-article-code)
