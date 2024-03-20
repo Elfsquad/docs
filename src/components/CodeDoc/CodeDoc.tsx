1813db8ecb3a75b811deb2eaf772852d7bf19f61
Johannes Heesterman <johannes@elfsquad.io>
2024-03-20 08:42:12 +0100

    activate on scroll

--- src/components/CodeDoc/CodeDoc.tsx
+++ src/components/CodeDoc/CodeDoc.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MDXPre from '@docusaurus/theme-classic/lib/theme/MDXComponents/Pre'
 import CodeBlock from '@theme/CodeBlock';
 import { CodeDocSection } from './CodeDocSection';
-import styles from './CodeBlock.module.css';
 
 
 export const CodeDoc = ({children}) => {
@@ -15,20 +14,37 @@ export const CodeDoc = ({children}) => {
 
     const codeBlock = childElements.find(child => child.type === CodeBlock || child.type === MDXPre);
     const sections = childElements.filter(child => child.type === CodeDocSection );
-    
-    const onSectionClick = (e, index) => {
-        const highlight = e?.props.highlight ?? '';
+    const sectionRefs = sections.map(() => React.useRef()) as React.RefObject<HTMLDivElement>[];
+
+    const activateSection = (index) => {
+        if (index < 0 || index >= sections.length) return;
         setActiveSection(index);
-        setMetadataString(highlight);        
-    }
+        setMetadataString(sections[index].props.highlight);
+    };
+
+    const onScroll = () => {
+        const scrollY = window.scrollY;
+        const sectionIndex = sectionRefs.findIndex(sectionRef => {
+            return sectionRef.current.offsetTop > scrollY;
+        });
+        activateSection(sectionIndex);        
+    };
+        
+    useEffect(() => {
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
 
     return <div className="grid grid-cols-2 gap-8">
         <div>
             {
                 sections.map((section, index) => {
                     return <div 
-                        className={`rounded p-4 hover:bg-adaptable-green cursor-pointer ${activeSection == index ? 'bg-adaptable-green': ''}`} 
-                        onClick={() => onSectionClick(section, index)}>
+                        key={index}
+                        ref={sectionRefs[index]}
+                        className={`CodeBlockSection rounded p-4 hover:bg-adaptable-green cursor-pointer ${activeSection == index ? 'bg-adaptable-green': ''}`} 
+                        onClick={() => activateSection(index)}>
                         <CodeDocSection {...section.props}></CodeDocSection>
                     </div>
                 })            
