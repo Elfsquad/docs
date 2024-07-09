da01800c4eded535142129ccf84497fc34256d7f
Stan van Rooy <me@stanvanrooy.com>
2024-07-09 10:43:14 +0200

    docs: add @elfsquad/custom-scripting docs

---
+++ docs/extensions/custom-scripting/methods/api.ts
@@ -0,0 +1,31 @@
+export default [
+  {
+    "methodName": "fetch",
+    "description": "Send an API request to the Elfsquad API. The url parameter is always prefixed with the base URL of the Elfsquad API.",
+    "example": {
+      "content": "const { status, body } = await api.fetch('/api/2/features');\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "url",
+        "type": "string",
+        "description": "The URL to send the request to.",
+        "required": true,
+        "parameters": []
+      },
+      {
+        "name": "init",
+        "type": "unknown",
+        "description": "The request options. This will be directly passed on to the native fetch function.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "Promise<FetchResponse>",
+      "description": ""
+    }
+  }
+]
