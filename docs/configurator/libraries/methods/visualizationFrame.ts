3d5b4cc2a4c31c54e2e0e5f5dedeae4c4cecd0ac
elfsquad-system-user <152181729+elfsquad-system-user@users.noreply.github.com>
2025-01-30 13:46:46 +0100

    Update documentation for @elfsquad/third-party-visualization (#84)
    Co-authored-by: GitHub Actions <actions@github.com>

---
+++ docs/configurator/libraries/methods/visualizationFrame.ts
@@ -0,0 +1,271 @@
+export default [
+  {
+    "methodName": "constructor",
+    "description": "Initializes a new instance of the third-party visualization.",
+    "example": {
+      "content": "const visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "options",
+        "type": "VisualizationFrameOptions",
+        "description": "The options used to initialize the showroom.",
+        "required": true,
+        "parameters": [
+          {
+            "name": "container",
+            "type": "HTMLElement | string",
+            "description": "The container to render the iframe element in. Accepts either a HTMLElement or a query selector string.",
+            "required": true
+          },
+          {
+            "name": "url",
+            "type": "string",
+            "description": "The URL where the third party visualization client is hosted.",
+            "required": true
+          }
+        ]
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": "A new instance of the VisualizationFrame class."
+    }
+  },
+  {
+    "methodName": "getNativeElement",
+    "description": "Retrieves the native iframe element.",
+    "example": {
+      "content": "const visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nconst iframe = visualization.getNativeElement();\n",
+      "language": "typescript"
+    },
+    "parameters": [],
+    "deprecated": null,
+    "returns": {
+      "type": "HTMLIFrameElement",
+      "description": "The native iframe element."
+    }
+  },
+  {
+    "methodName": "onTriggerConfigurationUpdate",
+    "description": "Registers a callback function to be invoked when a configuration update is triggered.",
+    "example": {
+      "content": "const visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onTriggerConfigurationUpdate(data => {\n  console.log('Update triggered. Please re-send the configuration');\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "TriggerConfigurationUpdateCallback",
+        "description": "The callback function to be called when a configuration update is triggered.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onUpdateRequirement",
+    "description": "Registers a callback function to be invoked when a requirement should be updated.",
+    "example": {
+      "content": "const updateRequirement = (data) => {\n  // your update requirement logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onUpdateRequirement(data => {\n  updateRequirement(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "UpdateRequirementCallback",
+        "description": "The callback function to be called when a requirement should be updated.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onUpdateRequirements",
+    "description": "Registers a callback function to be invoked when multiple requirements should be updated.",
+    "example": {
+      "content": "const updateRequirements = (data) => {\n  // your update requirements logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onUpdateRequirements(data => {\n  updateRequirements(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "UpdateRequirementsCallback",
+        "description": "The callback function to be called when multiple requirements should be updated.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onUpdateImageValue",
+    "description": "Registers a callback function to be invoked when an image value should be updated.",
+    "example": {
+      "content": "const updateImageValue = (data) => {\n  // your update image value logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onUpdateImageValue(data => {\n  updateImageValue(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "UpdateImageValueCallback",
+        "description": "The callback function to be called when an image value should be updated.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onUpdateTextValue",
+    "description": "Registers a callback function to be invoked when a text value should be updated.",
+    "example": {
+      "content": "const updateTextValue = (data) => {\n  // your update text value logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onUpdateTextValue(data => {\n  updateTextValue(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "UpdateTextValueCallback",
+        "description": "The callback function to be called when a text value should be updated.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onUpdateLinkedConfigurationCardinality",
+    "description": "Registers a callback function to be invoked when a linked configuration cardinality should be updated.",
+    "example": {
+      "content": "const updateLinkedConfigurationCardinality = (data) => {\n  // your update linked configuration cardinality logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onUpdateLinkedConfigurationCardinality(data => {\n  updateLinkedConfigurationCardinality(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "UpdateLinkedConfigurationCardinalityCallback",
+        "description": "The callback function to be called when a linked configuration cardinality should be updated.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onDragStarted",
+    "description": "Registers a callback function to be invoked when a drag operation is started.",
+    "example": {
+      "content": "const dragStarted = (data) => {\n  // your drag started logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onDragStarted(data => {\n  dragStarted(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "DragStartedCallback",
+        "description": "The callback function to be called when a drag operation is started.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "onRemoveLinkedConfiguration",
+    "description": "Registers a callback function to be invoked when a linked configuration should be removed.",
+    "example": {
+      "content": "const removeLinkedConfiguration = (data) => {\n  // your remove linked configuration logic here\n};\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\nvisualization.onRemoveLinkedConfiguration(data => {\n  removeLinkedConfiguration(data);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "callback",
+        "type": "RemoveLinkedConfigurationCallback",
+        "description": "The callback function to be called when a linked configuration should be removed.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "sendConfigurationUpdated",
+    "description": "Trigger a configuration update in the third-party visualization.",
+    "example": {
+      "content": "import { ConfiguratorContext, Configuration } from '@elfsquad/configurator';\nconst context = new ConfiguratorContext({ ... });\n\nconst visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\n\ncontext.onUpdate((evt: CustomEvent<Configuration>) => {\n  visualization.sendConfigurationUpdated(evt.detail);\n});\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "configuration",
+        "type": "Configuration",
+        "description": "The configuration to send to the third-party visualization.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  },
+  {
+    "methodName": "sendStepChanged",
+    "description": "Trigger a step changed event in the third-party visualization.",
+    "example": {
+      "content": "const visualization = new VisualizationFrame({ container: '#showroom', url: 'https://...' });\n\nconst step = { ... };\n\nvisualization.sendStepChanged(step);\n",
+      "language": "typescript"
+    },
+    "parameters": [
+      {
+        "name": "step",
+        "type": "ConfigurationStep",
+        "description": "The step to send to the third-party visualization.",
+        "required": true,
+        "parameters": []
+      }
+    ],
+    "deprecated": null,
+    "returns": {
+      "type": "void",
+      "description": ""
+    }
+  }
+]
